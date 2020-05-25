af53c875b2b1e9d9fa88409bff7bdb5e68cf297a
r00t <algocerebro@gmail.com>
2020-05-25 16:53:59 +0600

    Under construction

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={require("../src/dpb.png")} className="dpb" alt="logo" />
+        <p style={{fontSize: '25px'}}>
+          ওয়েবসাইটের কাজ চলছে...
         </p>
-        <a
+        <p style={{fontSize: '30px'}}>
+          প্লাজমা ডোনার এখনই পেতে ফেসবুক গ্রুপে জয়েন করুন
+        </p>  
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
